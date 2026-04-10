6a631ef9b886b770c2b8db1b9f9452c93d39144d
mru2-loves-computers <waldi240574@gmail.com>
2026-04-10 11:33:01 +0100

    change units button

--- react/react-app/src/App.jsx
+++ react/react-app/src/App.jsx
@@ -4,28 +4,35 @@ import './App.css'
 import CurrentWeather from './components/currentWeatherComponent';
 
 function App({coords}) {
-  const [ weather, setWeather ] = useState([]);
+  const [ units, setUnits ] = useState('metric');
+  const [ current, setCurrent ] = useState(null);
 
   const config = {
-    lat: '53.53412623985714',
-    lon: '-1.1123927605090957',
-    units: 'metric',
+    lat: coords?.lat || '53.53412623985714',
+    lon: coords?.lng || '-1.1123927605090957',
+    units: units,
     APIkey: 'f0ce22eb3cfa24b1b9be9e02ea65467d'
+  }
+  const getUnits = () => {
+    units==='metric'? setUnits('imperial') : setUnits('metric');
   }
   useEffect(() => {
     const getWeather = async () => {
-      const response = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${config.lat}&lon=${config.lon}&appid=${config.APIkey}`)
-      setWeather(response.data);
+      const response = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${config.lat}&lon=${config.lon}&units=${config.units}&appid=${config.APIkey}`)
+      setCurrent(response.data.current)
       console.log(response.data);
     }
+
     getWeather()
-  },[])
+  },[ units, config.lat, config.lon ])
 
   return (
     <>
       <h1>App ready</h1>
       <p>{coords ? `Lat: ${coords.lat}, Lng: ${coords.lng}` : "Locating..."}</p>
-      <CurrentWeather current={ weather.current } />
+      { current && <CurrentWeather current={ current } />}
+
+      <button onClick={getUnits}>Imperial</button>
     </>
   )
 }
--- react/react-app/src/components/currentWeatherComponent.jsx
+++ react/react-app/src/components/currentWeatherComponent.jsx
@@ -1,11 +1,14 @@
 
 
 function CurrentWeather({current}) {
+  console.log("Current: ",current);
+
+  if(!current) return <p>Loading weather...</p>
 
   return (
-    <>
-
-    </>
+    <div className="current-weather">
+      <h1>{ current.temp }</h1>
+    </div>
   )
 }
 
